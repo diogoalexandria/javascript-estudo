b336bafe3ce463ce1dce1ecedb37599ff505c38c
Diogo Alexnadria <diogoagrasmsdc@gmail.com>
2018-10-08 13:54:10 -0300

    Aula 3

--- js/principal.js
+++ js/principal.js
@@ -1,37 +1,53 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var arrayDePacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+for(var i = 0; i < arrayDePacientes.length; i++){
+    var paciente = arrayDePacientes[i];
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-var tdImc = paciente.querySelector(".info-imc");
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-var pesoValido = true;
-var alturaValida = true;
+    var tdImc = paciente.querySelector(".info-imc");
 
-if(peso <= 0 ||  peso >= 300){
-    pesoValido = false;  
+    var pesoValido = true;
+    var alturaValida = true;
+
+    if(peso <= 0 ||  peso >= 300){
+        pesoValido = false;  
+    }
+
+    if(altura <= 0 || altura >= 3.00){
+        alturaValida = false;
+    }
+
+    if(pesoValido && alturaValida){
+        var imc = peso / (altura * altura);    
+        tdImc.textContent = imc.toFixed(2);
+
+    } else if(!pesoValido && alturaValida){
+        console.log = ("Peso inválido.");
+        tdImc.textContent = "Peso inválido.";
+        //paciente.style.backgroundColor = "lightcoral";
+        paciente.classList.add("paciente-invalido");
+
+    } else if(pesoValido && !alturaValida){
+        console.log = ("Altura inválida");
+        tdImc.textContent = "Altura inválida.";
+        //paciente.style.backgroundColor = "lightcoral";
+        paciente.classList.add("paciente-invalido");
+
+    } else {
+        console.log = ("Peso e altura inválidas.");
+        tdImc.textContent = "Peso inválido e altura inválida.";
+        //paciente.style.backgroundColor = "lightcoral";
+        paciente.classList.add("paciente-invalido");
+
+    }
 }
 
-if(altura <= 0 || altura >= 3.00){
-    alturaValida = false;
-}
 
-if(pesoValido && alturaValida){
-    var imc = peso / (altura * altura);    
-    tdImc.textContent = imc;
-} else if(!pesoValido && alturaValida){
-    console.log = ("Peso inválido.");
-    tdImc.textContent = "Peso inválido.";
-} else if(pesoValido && !alturaValida){
-    console.log = ("Altura inválida");
-    tdImc.textContent = "Altura inválida.";
-} else {
-    console.log = ("Peso e altura inválidas.");
-    tdImc.textContent = "Peso inválido e altura inválida.";
-}
